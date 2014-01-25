e00477ab3dbf4e07eae188658936625f6bebd0a0
vegeta <vegeta@gmail.com>
2014-01-25 19:24:51 +0800

    Add create function and remove index from url

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -31,4 +31,24 @@ class News extends CI_controller {
 		$this->load->view('news/view', $data);
 		$this->load->view('templates/footer');
 	}
+
+	public function create()
+	{
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$data['title'] = 'Create a news item';
+
+		$this->form_validation->set_rules('title', 'Title', 'required');
+		$this->form_validation->set_rules('text', 'Content', 'required');
+
+		if ($this->form_validation->run() === false) {
+			$this->load->view('templates/header', $data);
+			$this->load->view('news/create');
+			$this->load->view('templates/footer');
+		} else {
+			$this->news_model->set_news();
+			$this->load->view('news/success');
+		}
+	}
 }
---
+++ application/views/news/create.php
@@ -0,0 +1,13 @@
+<h2>Create news item</h2>
+<?php echo validation_errors(); ?>
+<?php echo form_open('news/create'); ?>
+	<label for="title">Title</label>
+	<input type="input" name="title" />
+	<br>
+	<label for="text">Content</label>
+	<textarea name="text"></textarea>
+	<br>
+
+	<input type="submit" name="submit" value="Create" />
+
+</form>
